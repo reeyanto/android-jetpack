d90f7cec8f6dccf0add333db1acd43502fd010c5
Riyanto <riyanto.droider@gmail.com>
2021-01-13 07:53:30 +0700

    Menambahkan fitur databinding

--- app/src/main/java/com/riyanto/androidjetpackviewmodel/MainActivity.java
+++ app/src/main/java/com/riyanto/androidjetpackviewmodel/MainActivity.java
@@ -9,20 +9,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.riyanto.androidjetpackviewmodel.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding activityMainBinding;
     private MainViewModel mainViewModel;
-    private TextView label;
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // init view bernama button dan label
-        button = findViewById(R.id.button);
-        label  = findViewById(R.id.label);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = activityMainBinding.getRoot();
+        setContentView(view);
 
         // instansiasi kelas MainViewModel
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
@@ -31,12 +30,12 @@ public class MainActivity extends AppCompatActivity {
         mainViewModel.getNama().observe(this, new Observer<String>() {
             @Override
             public void onChanged(String s) {
-                label.setText(s);
+                activityMainBinding.label.setText(s);
             }
         });
 
         // saat button diklik, ubah data pada MainViewModel (jalankan method setNama())
-        button.setOnClickListener(new View.OnClickListener() {
+        activityMainBinding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mainViewModel.setNama();
